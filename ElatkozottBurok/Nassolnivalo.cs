84026a9544bf50b8535408db7ca80b4710b458be
Kover Marcell <1228marci@google.com>
2026-09-15 10:10:59 +0200

    proba

--- ElatkozottBurok/Nassolnivalo.cs
+++ ElatkozottBurok/Nassolnivalo.cs
@@ -4,7 +4,7 @@ namespace ElatkozottBurok
 {
     public class Nassolnivalo
     {
-        private string nev;
+        private string nev = string.Empty;
         private int koffeinLoket;
         private int stresszOldas;
         private int ar;
@@ -20,7 +20,7 @@ namespace ElatkozottBurok
         public string Nev
         {
             get => nev;
-            set { if (nev== "" || nev == null)
+            set { if (string.IsNullOrWhiteSpace(value))
                 {
                     nev = "ismeretlen nassolnivalo";
                 } else { nev = value; }}
@@ -29,9 +29,9 @@ namespace ElatkozottBurok
         public int KoffeinLoket
         {
             get => koffeinLoket;
-            set { if (koffeinLoket < 0) { 
+            set { if (value < 0) { 
                 koffeinLoket = 0; 
-                } else if (koffeinLoket > 50) { 
+                } else if (value > 50) { 
                     koffeinLoket = 50; 
                 } else { koffeinLoket = value; } }
         }
@@ -39,9 +39,9 @@ namespace ElatkozottBurok
         public int StresszOldas
         {
             get => stresszOldas;
-            set { if (stresszOldas < 0) { 
+            set { if (value < 0) { 
                 stresszOldas = 0; 
-                } else if (stresszOldas > 30) { 
+                } else if (value > 30) { 
                     stresszOldas = 30; 
                 } else { stresszOldas = value; } }
         }
@@ -49,7 +49,7 @@ namespace ElatkozottBurok
         public int Ar
         {
             get => ar;
-            set { if (ar < 0) { 
+            set { if (value < 100) { 
                 ar = 100; 
                 } else { ar = value; } }
         }
