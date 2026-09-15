53fad7e828bbd7cad679d5c97e423e1a685f4964
Kover Marcell <1228marci@google.com>
2026-09-15 08:22:39 +0200

    kezdetek

--- ElatkozottBurok/Nassolnivalo.cs
+++ ElatkozottBurok/Nassolnivalo.cs
@@ -4,6 +4,44 @@ namespace ElatkozottBurok
 {
     public class Nassolnivalo
     {
+        private string nev;
+        private int koffeinLoket;
+        private int stresszOldas;
+        private int ar;
 
+        public Nassolnivalo(string nev, int koffeinLoket, int stresszOldas, int ar)
+        {
+            this.Nev = nev;
+            this.koffeinLoket = koffeinLoket;
+            this.stresszOldas = stresszOldas;
+            this.ar = ar;
+        }
+
+        public string Nev
+        {
+            get => nev;
+            set { if (nev== "" || nev == null)
+                {
+                    nev = "ismeretlen nassolnivalo";
+                } else { nev = value; }}
+        }
+
+        public int KoffeinLoket
+        {
+            get { return koffeinLoket; }
+            set { koffeinLoket = value; }
+        }
+
+        public int StresszOldas
+        {
+            get { return stresszOldas; }
+            set { stresszOldas = value; }
+        }
+
+        public int Ar
+        {
+            get { return ar; }
+            set { ar = value; }
+        }
     }
 }
